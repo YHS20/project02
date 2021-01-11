135e8ab5dda82ba6d3ebf10ac2cce7e5e91f9d5c
YHS20 <20sueyun@gmail.com>
2021-01-11 15:11:26 +0900

    폰트적용시도

--- 대시보드추가/프로젝트2(대시보드추가)/MathMedics/src/mathmedics/Main.java
+++ 대시보드추가/프로젝트2(대시보드추가)/MathMedics/src/mathmedics/Main.java
@@ -4,18 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Font.loadFont(getClass().getResourceAsStream("Roboto-Thin.ttf"),14);
+
         Parent root = FXMLLoader.load(getClass().getResource("view/sidebar.fxml"));
         primaryStage.setTitle("Math Medics");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
